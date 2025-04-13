8898da9421c48c78cb1d78c33aee60cccffc4f56
David Weiss <davidwe@mta.ac.il>
2025-04-13 16:19:32 +0300

    some more changes to the architecture

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
-import * as userController from '../logic/userController.js';
+import * as userController from '../controller/userController.js';
+import * as pollController from '../controller/pollController.js';
 
 const router = Router();
 
+// Create a new user
 router.post('/', userController.createUser);
+
+// Vote on a poll
+router.post('/vote/:id', userController.voteOnPoll);
+
+// Get all votes by a user
+router.get('/votes/:username', userController.userVotes);
 
 export default router;
